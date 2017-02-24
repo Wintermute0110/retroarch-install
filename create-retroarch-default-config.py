d336f2a44ac22ac7c251f4f23b9141d80330431e
Wintermute0110 <wintermute0110@gmail.com>
2017-02-25 01:47:18 +0900

    Update configuration to Retroarch release 1.4.1

--- create-retroarch-default-config.py
+++ create-retroarch-default-config.py
@@ -3,13 +3,13 @@
 
 # Configuration  --------------------------------------------------------------
 # >> Configure paths to your system
-conf_source_path       = '/home/kodi/bin/source-retroarch/retroarch.cfg'
-conf_dest_path         = '/home/kodi/.config/retroarch/retroarch.cfg'
-conf_dest_initial_path = '/home/kodi/.config/retroarch/retroarch_initial.cfg'
+conf_source_path       = '~/bin/source-retroarch/retroarch-1.4.1.cfg'
+conf_dest_path         = '~/.config/retroarch/retroarch.cfg'
+conf_dest_initial_path = '~/.config/retroarch/retroarch_initial.cfg'
 
 # >> Retroarch replaces absolute user home dirs by '~'
 retroarch_stuff_dir = '~/.retroarch'
-ROMs_dir            = '~/AEL-ROMs/'
+ROMs_dir            = '~/Emulation/'
 libretro_dir        = '~/bin/libretro/'
 
 # Import stuff  ---------------------------------------------------------------
@@ -53,27 +53,43 @@ def edit_option(filename, option_name, option_value):
   os.rename('temp.cfg', filename)
 
 # Main  -----------------------------------------------------------------------
+# --- Resolve ~ directories into /home/user/ ---
+conf_source_path = os.path.expanduser(conf_source_path)
+conf_dest_path = os.path.expanduser(conf_dest_path)
+conf_dest_initial_path = os.path.expanduser(conf_dest_initial_path)
+retroarch_stuff_dir = os.path.expanduser(retroarch_stuff_dir)
+ROMs_dir = os.path.expanduser(ROMs_dir)
+libretro_dir = os.path.expanduser(libretro_dir)
+print('conf_source_path        "{0}"'.format(conf_source_path))
+print('conf_dest_path          "{0}"'.format(conf_dest_path))
+print('conf_dest_initial_path  "{0}"'.format(conf_dest_initial_path))
+print('retroarch_stuff_dir     "{0}"'.format(retroarch_stuff_dir))
+print('ROMs_dir                "{0}"'.format(ROMs_dir))
+print('libretro_dir            "{0}"'.format(libretro_dir))
+
 # --- Check if config file already exists (never overwrite it) ---
 if os.path.isfile(conf_dest_path):
-  print('Config file "{0}" already exists'.format(conf_dest_path))
-  print('Aborting')
+  print('>> Config file "{0}" already exists'.format(conf_dest_path))
+  print('>> Aborting')
   sys.exit(1)
 
 # --- Copy retroarch.cfg ---  
-print("Copying Retroarch default config file...")
+print(">> Copying Retroarch default config file ...")
 shutil.copyfile(conf_source_path, conf_dest_path)
 
 # --- Edit paths on the order they appear in RGUI 'Settings' -- 'Directory' ---
-# >> Update to Retroarch 1.3.0
-# To have a look at orphan directories in retroarch.cfg: 
-#  $ less retroarch.cfg | grep directory
-#  $ less retroarch.cfg | grep path
-# There is a bug in Retroarch, some directories end in _path (for files) instead of _directory.
+print(">> Editing Retroarch configuration file ...")
+# >> Make sure this matches the directory creation in install-retroarch.
+# >> Updated to Retroarch 1.4.1
+# >> To have a look at orphan directories in retroarch.cfg:
+#    $ cat retroarch.cfg | grep directory
+#    $ cat retroarch.cfg | grep path
+# >> There is a bug in Retroarch, some directories end in _path (for files) instead of _directory.
 edit_option(conf_dest_path, 'system_directory',             os.path.join(retroarch_stuff_dir, 'system/'))
-edit_option(conf_dest_path, 'core_assets_directory',        os.path.join(retroarch_stuff_dir, 'core_assets/'))
+edit_option(conf_dest_path, 'core_assets_directory',        os.path.join(retroarch_stuff_dir, 'core_assets/')) # >> Named Downloads in Retroarch
 edit_option(conf_dest_path, 'assets_directory',             os.path.join(retroarch_stuff_dir, 'assets/'))
 edit_option(conf_dest_path, 'dynamic_wallpapers_directory', os.path.join(retroarch_stuff_dir, 'wallpapers/'))
-edit_option(conf_dest_path, 'boxarts_directory',            os.path.join(retroarch_stuff_dir, 'boxarts/'))
+edit_option(conf_dest_path, 'thumbnails_directory',         os.path.join(retroarch_stuff_dir, 'thumbnails/'))
 edit_option(conf_dest_path, 'rgui_browser_directory',       os.path.join(ROMs_dir))
 edit_option(conf_dest_path, 'rgui_config_directory',        os.path.join(retroarch_stuff_dir, 'configurations/'))
 edit_option(conf_dest_path, 'libretro_directory',           os.path.join(libretro_dir))
@@ -84,20 +100,23 @@ edit_option(conf_dest_path, 'cheat_database_path',          os.path.join(retroar
 edit_option(conf_dest_path, 'video_filter_dir',             os.path.join(retroarch_stuff_dir, 'video_filters/'))
 edit_option(conf_dest_path, 'audio_filter_dir',             os.path.join(retroarch_stuff_dir, 'audio_filters/'))
 edit_option(conf_dest_path, 'video_shader_dir',             os.path.join(retroarch_stuff_dir, 'shaders_cg/'))
+edit_option(conf_dest_path, 'recording_output_directory',   os.path.join(retroarch_stuff_dir, 'recording_output/'))
+edit_option(conf_dest_path, 'recording_config_directory',   os.path.join(retroarch_stuff_dir, 'recording_config/'))
 edit_option(conf_dest_path, 'overlay_directory',            os.path.join(retroarch_stuff_dir, 'overlays/'))
-edit_option(conf_dest_path, 'osk_overlay_directory',        os.path.join(retroarch_stuff_dir, 'osk_overlays/'))
 edit_option(conf_dest_path, 'screenshot_directory',         os.path.join(retroarch_stuff_dir, 'screenshots/'))
-edit_option(conf_dest_path, 'joypad_autoconfig_dir',        os.path.join(retroarch_stuff_dir, 'joypad_autoconfig/'))
+edit_option(conf_dest_path, 'joypad_autoconfig_dir',        os.path.join(retroarch_stuff_dir, 'joypad_autoconfig/')) # Named Input Autoconfig
 edit_option(conf_dest_path, 'input_remapping_directory',    os.path.join(retroarch_stuff_dir, 'input_remappings/'))
 edit_option(conf_dest_path, 'playlist_directory',           os.path.join(retroarch_stuff_dir, 'playlists/'))
 edit_option(conf_dest_path, 'savefile_directory',           os.path.join(retroarch_stuff_dir, 'savefiles/'))
 edit_option(conf_dest_path, 'savestate_directory',          os.path.join(retroarch_stuff_dir, 'savestates/'))
-edit_option(conf_dest_path, 'recording_output_directory',   os.path.join(retroarch_stuff_dir, 'recording_output/'))
-edit_option(conf_dest_path, 'recording_config_directory',   os.path.join(retroarch_stuff_dir, 'recording_config/'))
 edit_option(conf_dest_path, 'cache_directory',              os.path.join(retroarch_stuff_dir, 'cache/'))
 
 # --- These directories are not in 'Settings' -- 'Directory' --- in RGUI ---
-edit_option(conf_dest_path, 'resampler_directory',          os.path.join(retroarch_stuff_dir, 'resampler/'))
+# edit_option(conf_dest_path, 'resampler_directory',           os.path.join(retroarch_stuff_dir, 'nodir/'))
+# edit_option(conf_dest_path, 'core_options_path',             os.path.join(retroarch_stuff_dir, 'nodir/'))
+# edit_option(conf_dest_path, 'bundle_assets_src_path',        os.path.join(retroarch_stuff_dir, 'nodir/'))
+# edit_option(conf_dest_path, 'bundle_assets_dst_path',        os.path.join(retroarch_stuff_dir, 'nodir/'))
+# edit_option(conf_dest_path, 'bundle_assets_dst_path_subdir', os.path.join(retroarch_stuff_dir, 'nodir/'))
 
 # --- Other options -----------------------------------------------------------
 # >> For idead have a look at https://github.com/libretro/Lakka/blob/lakka/packages/libretro/retroarch/package.mk
@@ -107,7 +126,7 @@ edit_option(conf_dest_path, 'video_font_path', os.path.join(retroarch_stuff_dir,
 edit_option(conf_dest_path, 'video_font_size', '22.000000')
 edit_option(conf_dest_path, 'input_max_users', '2')
 edit_option(conf_dest_path, 'input_autodetect_enable', 'true')
-edit_option(conf_dest_path, 'video_fullscreen', 'true')
+# edit_option(conf_dest_path, 'video_fullscreen', 'true')
 edit_option(conf_dest_path, 'video_aspect_ratio_auto', 'true')
 edit_option(conf_dest_path, 'video_smooth', 'false')
 edit_option(conf_dest_path, 'video_threaded', 'true')
@@ -115,6 +134,8 @@ edit_option(conf_dest_path, 'video_gpu_screenshot', 'false')
 edit_option(conf_dest_path, 'audio_driver', 'alsathread')
 edit_option(conf_dest_path, 'input_driver', 'udev')
 
+# >> Separate savestates and savefiles per-core. Important because different cores can be used to
+#    run the same ROM.
 edit_option(conf_dest_path, 'sort_savefiles_enable', 'true')
 edit_option(conf_dest_path, 'sort_savestates_enable', 'true')
 
@@ -123,6 +144,9 @@ edit_option(conf_dest_path, 'sort_savestates_enable', 'true')
 edit_option(conf_dest_path, 'input_exit_emulator_btn', '10')
 edit_option(conf_dest_path, 'input_menu_toggle_btn', '12')
 
+# >> Keep old keys Z and X for nagivating Retroarch GUI
+edit_option(conf_dest_path, 'menu_unified_controls', 'true')
+
 # --- Copy newly edited retroarch.cfg into retroarch_initial.cfg as a backup ---
-print("Creating retroarch.cfg initial backup...")
+print(">> Backing up initial configuration file ...")
 shutil.copyfile(conf_dest_path, conf_dest_initial_path)
