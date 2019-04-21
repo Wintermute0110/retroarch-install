27a0d6f37e9d7b0e2635780f9f8a590cf1166c98
Wintermute0110 <wintermute0110@gmail.com>
2019-04-21 22:57:26 +0900

    Improve compile dependencies a bit.

--- install-build-dependencies.sh
+++ install-build-dependencies.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 #
 # Officially recomended in RetroArcg github wiki
-apt-get install build-essential make git curl pkg-config nvidia-cg-toolkit \
+#
+apt-get install \
+    build-essential \
+    make \
+    git \
+    curl \
+    pkg-config \
+    nvidia-cg-toolkit
+
+apt-get install \
     freeglut3-dev \
     mesa-common-dev \
+    libavcodec-dev \
+    libavdevice-dev \
+    libresvg-dev \
     libgbm-dev \
     libgl1-mesa-dev \
-    libgles1-mesa-dev \
     libgles2-mesa-dev \
     libglu1-mesa-dev \
     libsdl2-dev \
