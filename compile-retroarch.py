ef1bd8cc93cac157395b80bee9bd39f8007e268d
Wintermute0110 <wintermute0110@gmail.com>
2019-08-24 23:22:58 +0900

    Improve verbosity a bit.

--- compile-retroarch.py
+++ compile-retroarch.py
@@ -11,17 +11,19 @@ import common
 # Configuration  ---------------------------------------------------------------------------------
 configuration = common.read_config_file('configuration.xml')
 retroarch_version = 'tags/v{}'.format(configuration['Version'])
-print('retroarch_version "{0}"'.format(retroarch_version))
+print('>>> retroarch_version "{0}"'.format(retroarch_version))
 current_dir = os.getcwd()
-print('Current Working Directory "{}"'.format(current_dir))
+print('>>> Current Working Directory "{}"'.format(current_dir))
 
 # --- Switch source core to requested tag ---
+print('>>> Checking out Retroarch tag {}'.format(retroarch_version))
 os.chdir('libretro-super/retroarch')
 # common.run_command(['git', 'checkout', 'master'])
 common.run_command(['git', 'checkout', retroarch_version])
 os.chdir(current_dir)
 
 # --- Build ---
+print('>>> Bulding Retroarch...')
 os.chdir('libretro-super')
 common.run_command(['./retroarch-build.sh'])
 os.chdir(current_dir)
