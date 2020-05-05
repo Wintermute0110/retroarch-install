865138c36500172a26a86178b515b810c2028b3a
Wintermute0110 <wintermute0110@gmail.com>
2020-05-05 23:13:13 +0900

    New script to clone Dolphin repository.

---
+++ clone-dolphin.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3 -B
+
+# Clones the Dolphin repository. It is required to install necessary runtime files
+# into the retroarch system dir to execute the dolphin core.
+# https://libretro.readthedocs.io/en/latest/library/dolphin/
+
+# --- Python standard library --------------------------------------------------------------------
+import os
+
+# --- Import custom modules ----------------------------------------------------------------------
+import common
+
+# --- main ---------------------------------------------------------------------------------------
+common.run_command(['git', 'clone', '--depth=1', 'https://github.com/libretro/dolphin.git'])
