aa94ed5d03650dbbe15a02f765c5b2f689dcbc2b
Wintermute0110 <wintermute0110@gmail.com>
2021-09-19 00:51:02 +0900

    Fix this script.

--- diff-retroarch-config-changes.py
+++ diff-retroarch-config-changes.py
@@ -11,6 +11,8 @@ config = common.read_config_file('configuration.xml')
 
 # subprocess module does not support globbing, needs full qualified paths.
 # Create a link in place of '/home/kodi/.config/retroarch/' if needed.
-source_path = '{}/.config/retroarch/retroarch.initial.cfg'.format(config['HomeDir'])
-dest_path = '{}/.config/retroarch/retroarch.cfg'.format(config['HomeDir'])
-common.run_command(['diff', '-u', '--color', source_path, dest_path])
+source_path = '{}/retroarch.initial.cfg'.format(config['ConfigDir'])
+dest_path = '{}/retroarch.cfg'.format(config['ConfigDir'])
+print('Diffing "{}"'.format(source_path))
+print('with    "{}"'.format(dest_path))
+common.run(['diff', '-u', '--color', source_path, dest_path])
